2f0f4b841eb8f78ae9745faee8df9e64a386a9e8
sfandress <sfandress.prog@gmail.com>
2023-04-10 22:45:32 -0300

    Cambios generales de linkeo
    Se realizaron cambios en algunas clases en el header  para que no se superpongan en la hoja css de otras secciones. Tambien la ubicacion de los archivos de estilo e imagenes

--- js/validaciones.js
+++ js/validaciones.js
@@ -85,3 +85,12 @@ function validarCorreo(correo){
     }
 }
 
+
+function genPDF(){
+    var doc=new jsPDF();
+    let mensaje=document.getElementById("esc").value;
+    doc.text(20,20,mensaje);
+    doc.addPage();
+    doc.text(20,20,'Mi trabajo!!');
+    doc.save('Test.pdf');
+}
